aaeb45f6a798fe1820578a5f6a999ef1308c59f8
Pavel Kazantsev <pavel.kazantsev@anywayanyday.com>
2015-06-29 15:53:30 +0300

    Added a demo project

---
+++ TabbedSplitViewControllerDemo/TabbedSplitViewControllerDemo/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  TabbedSplitViewControllerDemo
+//
+//  Created by Pavel Kazantsev on 29/06/15.
+//  Copyright (c) 2015 Pavel Kazantsev. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+
+}
+
---
+++ TabbedSplitViewControllerDemo/TabbedSplitViewControllerDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TabbedSplitViewControllerDemo
+//
+//  Created by Pavel Kazantsev on 29/06/15.
+//  Copyright (c) 2015 Pavel Kazantsev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+        
+    }
+
+}
+
